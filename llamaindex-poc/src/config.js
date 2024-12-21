94a27125d2a5d127ca434cc3c56454ddf3f5625e
ajohn-wick <ajohnwick@mrwick.org>
2024-12-21 17:54:29 +0100

    Switched model in local bedrock for Anthropic Claude Haiku 3.5 + set Lambda runtime to Node@20

--- llamaindex-poc/src/config.js
+++ llamaindex-poc/src/config.js
@@ -1,6 +1,6 @@
 import { BEDROCK_MODELS } from "@llamaindex/community";
 
 export const config = {
-    "knowledgeBaseId": "",
-    "modelId": BEDROCK_MODELS.ANTHROPIC_CLAUDE_3_5_HAIKU.
+    "knowledgeBaseId": "RWQ2DRILT2",
+    "modelId": BEDROCK_MODELS.ANTHROPIC_CLAUDE_3_5_HAIKU
 };
